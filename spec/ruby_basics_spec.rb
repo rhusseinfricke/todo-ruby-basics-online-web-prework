b82dc8487a33fc87ef553ef0953b399dd0a52788
boisterous-design-4553 <reemimhussein@outlook.com>
2019-01-06 15:51:34 +0000

    Done.

--- spec/ruby_basics_spec.rb
+++ spec/ruby_basics_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require_relative '../lib/ruby_basics.rb'
+require_relative './lib/ruby_basics.rb'
 
 
 describe "ruby" do
@@ -8,36 +8,38 @@ describe "ruby" do
     it 'given two numbers returns quotient of the first number divided by the second number' do
       num1 = 42
       num2 = 7
-
+      quotient = num1/num2
+      puts quotient
       expect(division(num1,num2)).to eq(6)
     end
   end
 
-  describe '#assign_variable' do
+  describe(value) '#assign_variable' do
     it 'takes an argument of a persons name and assign it to a variable name' do
       value = "Bob"
+      puts #{value}
 
       expect(assign_variable(value)).to eq(value)
     end
   end
 
-  describe '#argue' do
+  describe(phrase) '#argue' do
     it 'accepts an argument and return that argument as is' do
       phrase = "I'm right and you are wrong!"
-
+      puts #{phrase}
       expect(argue(phrase)).to eq(phrase)
     end
   end
 
-  describe '#greeting' do
+  describe(greeting, name) '#greeting' do
     it 'takes two arguments' do
       greeting = "Hi there, "
       name = "Bobby!"
-
       expect{ greeting(greeting, name) }.to_not raise_error
+      end
     end
 
-    it 'raises an error with one argument' do
+    describe(greeting = Hi there) it 'raises an error with one argument' do
       greeting = "Hi there, "
 
       expect{ greeting(greeting)}.to raise_error
@@ -47,22 +49,29 @@ describe "ruby" do
   describe '#return_a_value' do
     it 'returns the phrase "Nice"' do
       expect(return_a_value).to eq("Nice")
+      return_a_value="Nice"
+      puts return_a_value
     end
   end
 
   describe '#last_evaluated_value' do
     it 'returns the phrase "expert"' do
       expect(last_evaluated_value).to eq("expert")
+      last_evaluated_value="expert"
+      puts last_evaluated_value
     end
   end
 
-  describe '#pizza_party' do
+  describe(pizza_party="cheese") '#pizza_party' do
     it 'returns "cheese" by default' do
       expect(pizza_party).to eq("cheese")
+      puts pizza_party
     end
+  end
 
-    it 'returns the argument it received' do
+    describe(pizza_party) it 'returns the argument it received' do
       expect(pizza_party("pepperoni")).to eq("pepperoni")
+      puts #{pizza_party}
     end
   end
 end
